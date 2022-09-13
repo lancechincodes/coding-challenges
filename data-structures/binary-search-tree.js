688eb7c50f3220dd0edb9c14a1b699362eed72f5
lancechincodes <lanceechin@gmail.com>
2022-09-13 02:05:55 -0500

    Add breadth first search method to traverse tree

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -59,6 +59,21 @@ class BinarySearchTree {
         return false
     }
 
+    // traverse tree with BFS (one horizontal level at a time)
+    breadthFirstSearch() {
+        let visited = [] // holds dequeued nodes' values 
+        let queue = [] // holds nodes
+        let current = this.root
+        queue.push(current)
+        while(queue.length !== 0) {
+            current = queue.shift() // FIFO
+            visited.push(current.value)
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }
+        return visited
+    }
+
 }
 
 const tree = new BinarySearchTree()
@@ -68,12 +83,20 @@ tree.insert(10)
 tree.insert(6)
 tree.insert(15)
 tree.insert(15)
-console.log(tree.insert(12))
+tree.insert(12)
+tree.insert(3)
+tree.insert(8)
+//        10
+//    6       15
+//  3   8    12
 
 // Testing find method
-console.log(tree.find(10)) // true
-console.log(tree.find(6)) // true
-console.log(tree.find(15)) // true
-console.log(tree.find(12)) // true
-console.log(tree.find(8)) // false
-console.log(tree.find(25)) // false
+tree.find(10) // true
+tree.find(6) // true
+tree.find(15) // true
+tree.find(12) // true
+tree.find(8) // false
+tree.find(25) // false
+
+// Testing breadth first search tree traversal 
+console.log(tree.breadthFirstSearch()) // [10,6,15,3,8,12]
