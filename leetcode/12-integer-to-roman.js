04fb56480deddefc7657a3dbb0f490c5eccfe2b2
lancechincodes <lanceechin@gmail.com>
2022-09-15 00:33:50 -0500

    Add integer to roman

---
+++ leetcode/12-integer-to-roman.js
@@ -0,0 +1,54 @@
+/*
+R: Input of integer, convert to roman numeral string
+E: 17 // XVII
+edge cases: IV - 4, IX - 9, XL - 40, XC - 90, CD - 400, CM - 900
+A: 1) Create a conversion table
+2) Iterate over Roman conversions
+3) Calculate the dividend of num divided the conversions from highest to lowest
+4) Once dividend is 1 or more continue
+5) Calculate modulus or remainder and set num to be mod
+6) Call convert function for as many times it divided smoothly (dividend)
+- within convert function take the key as a parameter and concat to the result string
+7) Return the result string
+
+*/
+
+// Time complexity: O(n^2) double for loop
+// Space complexity: O(n) conversion obj
+var intToRoman = function(num) {
+    // Make sure to add the six "edge case" instances into conversion object
+    let conversion = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+    
+    let result = ''
+    function convert(key) {
+        result = result.concat(key)
+    }
+    
+    let div, mod
+    for (let key in conversion) {
+        div = Math.floor(num / conversion[key])
+        if (div >= 1) {
+            mod = num % conversion[key]
+            num = mod
+            for (let i = 0; i < div; i++) {
+                convert(key)
+            }
+        }
+    }
+    
+    return result
+};
