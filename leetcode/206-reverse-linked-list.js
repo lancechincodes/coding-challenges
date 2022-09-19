ca69a772c334c4c3cb4fad638eeea4c4cea9ca10
lancechincodes <lanceechin@gmail.com>
2022-09-19 02:06:26 -0500

    Fix typo

--- leetcode/206-reverse-linked-list.js
+++ leetcode/206-reverse-linked-list.js
@@ -2,7 +2,7 @@
 // E: [1,2,3,4] --> [4,3,2,1]
 // A: Initialize prev and next pointers
 // While head exists
-    // Updata next, head.next, prev, and head (in that order)
+    // Update next, head.next, prev, and head (in that order)
 // Return prev (because it be the new head when the LL is reversed)
 
 // Time complexity: O(n)
