977d87fe0001db96d7f95510cc4c5086ac501b68
lancechincodes <lanceechin@gmail.com>
2022-10-08 21:52:31 -0500

    Add more optimal solution and approach (inside out approach with helper function that checks longest even and odd palindrome with ith as center

--- leetcode/5-longest-palindromic-substring.js
+++ leetcode/5-longest-palindromic-substring.js
@@ -12,11 +12,23 @@ Explanation: "aba" is also a valid answer.
 Input: s = "cbbd"
 Output: "bb"
 
+A: 
+1) Brute force
+    - Check all combinations (O(n^3))
+2) Inside out approach
+    - for loop going through each char in s
+        - even palindrome (check i-1 and i to be equal)
+        - odd palindrome (check i-1 and i+1 to be equal)
+            - if equal move left and right down 
+            - if not equal, break out of while loop
+        - set current max between even and odd (indices)
+        - set overall max between overall max and current max (indices)
+    - return s.slice(max indices)
 */
 
 // Too slow
 // TC: O(n^3)
-// SC: O(1)
+// SC: O(n), creating str
 var longestPalindrome = function(s) {
 
     // helper function
@@ -37,8 +49,30 @@ var longestPalindrome = function(s) {
     return longestSS    
 };
 
-
 // Optimize
-var longestPalindrome = function(s) {
+// Helper function to get indices of longest left and right palindrome with the ith element as the middle
+const getIndices = (left, right, s) => {
+    while (left >= 0 && right < s.length) {
+        if (s[left] !== s[right]) break
+        left--
+        right++
+    }
+    return [left + 1, right]
+}
 
+// TC: O(n^2), for every ith element worse case run n times
+// SC: O(n)
+var longestPalindrome = function(s) {
+    // base case
+    let max = [0,1]
+
+    for (let i = 0; i < s.length; i++) {
+        let even = getIndices(i - 1, i, s) // ex: "cbbd" --> bb
+        let odd = getIndices(i - 1, i + 1, s) // ex: "babad" --> bab
+        let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even
+
+        max = max[1] - max[0] > curMax[1] - curMax[0] ? max : curMax
+    }
+
+    return s.slice(max[0], max[1]) // non-inclusive in end index
 }
