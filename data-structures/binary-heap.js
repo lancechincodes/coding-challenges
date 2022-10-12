e4e9fcf507d4aaf6db6cee2cf59fe82c27da3354
lancechincodes <lanceechin@gmail.com>
2022-10-12 15:55:03 -0500

    Add max binary heap class + insert method

--- data-structures/binary-heap.js
+++ data-structures/binary-heap.js
@@ -20,8 +20,64 @@ Representing a Heap
 - Left child index: 2n + 1
 - Right child index: 2n + 2
     - where n is index of parent
-    
+
 - If given opposite (Have index n which the child)
     - Parent index: Math.floor((n-1)/2)
 
 */
+
+// Implementing max binary heap but min binary heap is similar
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [] // initialize as empty arr
+    }
+
+    insert(val) {
+        // 1) push the val into values property on the heap
+        this.values.push(val)
+        // 2) bubble up
+        this.bubbleUp()
+        return this.values
+    }
+
+    // helper function for insert
+    bubbleUp() {
+        let index = this.values.length - 1
+        let parentIndex = Math.floor((index-1) / 2)
+        while (this.values[parentIndex] < this.values[index]) {
+            // swap values at index and parent index
+            let temp = this.values[parentIndex]
+            this.values[parentIndex] = this.values[index]
+            this.values[index] = temp
+
+            // set new index and parent index
+            index = parentIndex
+            parentIndex = Math.floor((index-1) / 2)
+        }
+    }
+}
+
+// Testing methods
+// Create instance of binary heap
+let maxBinaryHeap = new MaxBinaryHeap()
+maxBinaryHeap.insert(41)
+maxBinaryHeap.insert(39)
+maxBinaryHeap.insert(33)
+maxBinaryHeap.insert(18)
+maxBinaryHeap.insert(27)
+maxBinaryHeap.insert(12) // [41,39,33,18,27,12]
+/*
+            41
+        39      33
+      18  27   12  
+*/
+
+// Testing insert method
+maxBinaryHeap.insert(55) // [55,39,41,18,27,12,33]
+/*
+            55
+        39       41
+      18  27   12  33
+*/
+maxBinaryHeap.insert(1) // [55,39,41,18,27,12,33,1]
+maxBinaryHeap.insert(45) // [55.45.41.39,27,12,33,1,18]
