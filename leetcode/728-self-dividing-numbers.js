947c8603095a29e9011fbc7137497926fcb862cf
lancechincodes <lanceechin@gmail.com>
2022-10-29 12:17:08 -0500

    Complete self dividing numbers (nested for loop)

---
+++ leetcode/728-self-dividing-numbers.js
@@ -0,0 +1,45 @@
+/*
+R: 
+Input - 2 ints
+Output - arr of all self-dividing numbers in range [left, right]
+
+E:
+Input: left = 1, right = 22
+Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+
+Input: left = 47, right = 85
+Output: [48,55,66,77]
+
+A:
+- Create result global variable
+- Loop from left to right (inclusive)
+    - stringify ith number
+    - set self-dividing to true
+    - Loop from first digit in number to last 
+        - check if ith num is NOT divisible by its digit(s)
+            - set self-dividing to false 
+            - break out of this loop
+    - if self-dividing remained true
+        - push ith number to result arr 
+- return result arr
+
+*/
+// TC: O(n * m) where n is difference between left and right and m is length of the stringifyed ith number
+// SC: O(n) storing result arr and storing string version of number
+var selfDividingNumbers = function(left, right) {
+
+    let result = []
+    for (let i = left; i <= right; i++) {
+        let strI = String(i)
+        let selfDividing = true
+        for (let j = 0; j < strI.length; j++) {
+            if (i % Number(strI[j]) !== 0) {
+                selfDividing = false
+                break;
+            }
+        }
+        if (selfDividing) result.push(i)
+    }
+
+    return result
+};
