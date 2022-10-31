c1984325226e0552530f6db95bd8c5810794b128
lancechincodes <lanceechin@gmail.com>
2022-10-31 18:00:00 -0500

    Complete brute force sol to three sum

---
+++ leetcode/15-three-sum.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+/*
+R: 
+Input - nums arr
+Output - arr of triplets that sum to 0 and are not at same index 
+
+E:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+A:
+1) Brute force solution - triple for loops
+*/
+
+// Brute force solution
+// TC: O(n^3)
+// SC: O(1)
+var threeSum = function(nums) {
+    const result = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (nums[i] + nums[j] + nums[k] === 0) {
+                    result.push([nums[i], nums[j], nums[k]])
+                }
+            }
+        }
+    }
+    return result
+};
+
+// test cases
+console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([0,1,1]))
+console.log(threeSum([0,0,0]))
