45cebedd56ee1b32d32744d66d8951655623b5ce
lancechincodes <lanceechin@gmail.com>
2022-11-11 13:12:04 -0600

    Add problem statement to remove LL elements

---
+++ leetcode/203-remove-linked-list-elements.js
@@ -0,0 +1,15 @@
+/*
+R: Given head of a LL and int val - remove all nodes of LL whose value is equal to parameter val
+
+E: 
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Input: head = [], val = 1
+Output: []
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+A: 
+*/
