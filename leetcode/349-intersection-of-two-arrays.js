d66ddb04c082ff1e4c9a48188d0654348beb8c5d
lancechincodes <lanceechin@gmail.com>
2022-12-14 11:12:54 -0600

    Add problem statement and examples to 349

---
+++ leetcode/349-intersection-of-two-arrays.js
@@ -0,0 +1,12 @@
+/*
+R: Input - Two int arrays nums1 and num2
+Output - Return array of their intersection (unique elements and you may return result in any order)
+
+E: 
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+*/
