8b7cbf71b40bad88a2fa874128c83e62c0df96c8
lancechincodes <lanceechin@gmail.com>
2022-12-27 12:56:54 -0600

    Add problem statement to 1528 shuffle string

---
+++ leetcode/1528-shuffle-string.js
@@ -0,0 +1,15 @@
+/*
+R: 
+Input - string s and int arr indices of same length
+Output - shuffled string in which character at the ith pos in s moves to indices[i] in the shuffled string
+
+E:
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+
+*/
